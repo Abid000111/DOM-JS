7f4a58549a3f489832ce7238a90fd07defb13805
Abid Shahrier <abidofficial0001@gmail.com>
2024-01-07 10:34:12 +0600

    Added initial files

--- script.js
+++ script.js
@@ -93,6 +93,9 @@
 // // Dom Manipulating
 
 
+
+
+
 // // properties
 
 // // tagName: it returns the element's tag name
@@ -113,3 +116,90 @@
 
 // // textContent: returns textual content even for hidden elements
 // console.dir(document.querySelector(".heading").textContent);
+
+
+
+
+
+// // Attributes
+
+// // getAttributes(attr): to get the attribute value
+// let div = document.querySelector("div");
+// let id = div.getAttribute("id");
+// console.log(id);
+
+// let classAttribute = div.getAttribute("class");
+// console.log(classAttribute);
+// let name = div.getAttribute("name");
+// console.log(name);
+
+// // setAttribute(attr, value): to set the attribute value
+// let div = document.querySelector("div");
+// console.log(div.setAttribute("name", "aboutDOM"));   // this will show undefined in console but the attribute value has changed
+
+// // node.style: to set or change style
+// let h2 = document.querySelector("#heading2");
+// console.log(h2.style);
+// h2.style.textDecoration = "underline";
+
+
+
+
+
+// // node.classList(): it has many attributes such as add, remove and many more to manipulate DOM
+// document.querySelector("#definition").classList.add("newclass");    // It will add new class("newclass") to the element but won't overwrite the existing class
+
+
+
+
+
+// // insert element
+
+// // node.prepend(el): adds at the start of node (inside)
+// let newHeading = document.createElement("h1");
+// newHeading.innerHTML = "<i>Hi, I am inserted by JS</i>";
+// document.querySelector("body").prepend(newHeading);
+
+// // node.append(el): adds at the end of node (inside)
+// let button = document.createElement("button");
+// button.innerText = "Subscribe";
+// button.style.color = "white";
+// button.style.backGroundColor = "red";
+// document.querySelector("#definition").append(button);
+
+// // node.before(el): adds before the node (outside)
+// let para = document.createElement("p");
+// para.innerHTML = "<p><b>## Definition of DOM point by point:</b></p>";
+// document.querySelector("#definition").before(para);
+
+// // node.after(el): adds after the node (outside)
+// let para = document.createElement("p");
+// para.innerHTML = "<p><b>## Definition of DOM point by point:</b></p>";
+// document.querySelector("#definition").after(para);
+
+// // node.appendChild(): adds a node as a child
+// const newParagraph = document.createElement('p');
+// newParagraph.textContent = 'This is a new paragraph';
+// const parentElement = document.getElementById('definition');
+// parentElement.appendChild(newParagraph);
+
+
+
+
+
+
+// // delete element
+
+// // node.remove(): removes the node
+// let para = document.createElement("p");
+// para.innerHTML = "<p><b>## Definition of DOM point by point:</b></p>";
+// document.querySelector("#definition").before(para);
+// para.remove()
+
+// // node.removeChild(): removes the node from parent
+// // Get the parent element from which you want to remove a child
+// const parentElement = document.getElementById('definition');
+// // Get the child element that you want to remove
+// const childToRemove = document.getElementById('childToRemove');
+// // Remove the child element from its parent
+// parentElement.removeChild(childToRemove);
